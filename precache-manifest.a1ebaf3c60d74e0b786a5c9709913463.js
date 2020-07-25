bfbb114fc9e966eac8891485c7610a5412967511
Alex <militara106@gmail.com>
2020-07-25 16:36:31 -0400

    Updates

---
+++ precache-manifest.a1ebaf3c60d74e0b786a5c9709913463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2370c93bd84fbf8ab34e0b43f2f88861",
+    "url": "/Jewel/index.html"
+  },
+  {
+    "revision": "57204a7d9f5aede6839b",
+    "url": "/Jewel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "274a06787aa5e4c78bdf",
+    "url": "/Jewel/static/js/2.a2589592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jewel/static/js/2.a2589592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57204a7d9f5aede6839b",
+    "url": "/Jewel/static/js/main.926c8428.chunk.js"
+  },
+  {
+    "revision": "ab109bb6a9e248d8f4b5",
+    "url": "/Jewel/static/js/runtime-main.ef999ad0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jewel/static/media/logo.5d5d9eef.svg"
+  }
+]);
