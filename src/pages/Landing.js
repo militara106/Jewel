f95ddd50b804339e2cf555c8dd4295583fc12d79
Alex <militara106@gmail.com>
2020-07-26 20:49:29 -0400

    react-reveal animations added to landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,51 +3,58 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Fade from "react-reveal/Fade";
 
 function Landing() {
   return (
     <Container className="centered landing">
-        <div className="bg"></div>
+      <div className="bg"></div>
       {/* Logo */}
-      <div className="Logo"></div>
+      <Fade top>
+        <div className="landingLogoBG">
+          <div className="landingLogo">J</div>
+        </div>
+      </Fade>
 
       <Container className="textBox">
-        {/* App Title */}
-        <Row>
-          <Col>
-            <div className="landingTitle">Jeweled</div>
-          </Col>
-        </Row>
+        <Fade bottom>
+          {/* App Title */}
+          <Row>
+            <Col>
+              <div className="landingTitle">Jeweled</div>
+            </Col>
+          </Row>
 
-        {/* Sub Title */}
-        <Row>
-          <Col>
-            <div className="landingSub">The Dating App</div>
-          </Col>
-        </Row>
+          {/* Sub Title */}
+          <Row>
+            <Col>
+              <div className="landingSub">The Dating App</div>
+            </Col>
+          </Row>
 
-        {/* Description */}
-        <Row>
-          <Col>
-            <div className="landingDesc">
-              Transitioning into a more meaningful relationship
-            </div>
-          </Col>
-        </Row>
+          {/* Description */}
+          <Row>
+            <Col>
+              <div className="landingDesc">
+                Transitioning into a more meaningful relationship
+              </div>
+            </Col>
+          </Row>
 
-        {/* Sign In */}
-        <Row>
-          <Col>
-            <Button className="signIn borderButton">Sign In</Button>
-          </Col>
-        </Row>
+          {/* Sign In */}
+          <Row>
+            <Col>
+              <Button className="signIn borderButton">Sign In</Button>
+            </Col>
+          </Row>
 
-        {/* Sign Up */}
-        <Row>
-          <Col>
-            <Button className="signUp borderlessButton">Sign Up</Button>
-          </Col>
-        </Row>
+          {/* Sign Up */}
+          <Row>
+            <Col>
+              <Button className="signUp borderlessButton">Sign Up</Button>
+            </Col>
+          </Row>
+        </Fade>
       </Container>
     </Container>
   );
